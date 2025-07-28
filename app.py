589cce55979e3f324cd7ff862f596d53e32ee5c5
Ｄʜʀᴜᴠᴀ Ｋᴜᴍᴀʀ <ffg288844@gmail.com>
2025-07-28 17:47:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -30,46 +30,54 @@ def create_complete_player_activity(user_id):
     activity = SelectedItems()
     
     # ===== SelectedItems Fields =====
-    # Basic appearance (fields 1-6)
+    # Field 1: avatar_id (uint32)
     activity.avatar_id = 1001 + (user_id % 10)
-    activity.skin_color = 2 + (user_id % 3)
-    activity.clothes.extend([101, 102, 103, 104 + user_id % 5])
-    activity.banner_id = 5001 + (user_id % 7)
-    activity.head_pic = 2001 + (user_id % 4)
     
-    # Loadouts (field 4 - repeated LoadoutInfo)
+    # Field 2: skin_color (uint32)
+    activity.skin_color = 2 + (user_id % 3)
+    
+    # Field 3: clothes (repeated uint32)
+    activity.clothes.extend([101, 102, 103, 104 + user_id % 5])
+    
+    # Field 4: loadouts (repeated LoadoutInfo)
     for i in range(1, 4):
         loadout = activity.loadouts.add()
         loadout.loadout_id = i
         loadout.loadout_num = i
         loadout.is_free_play = (i % 2 == 0)
     
-    # Slots (field 7 - repeated uint32)
+    # Field 5: banner_id (uint32)
+    activity.banner_id = 5001 + (user_id % 7)
+    
+    # Field 6: head_pic (uint32)
+    activity.head_pic = 2001 + (user_id % 4)
+    
+    # Field 7: slots (repeated uint32)
     activity.slots.extend([700 + i for i in range(5)])
     
-    # Emotes (field 8 - ChoosedEmotes)
+    # Field 8: emotes (ChoosedEmotes)
     for i in range(1, 5):
         emote = ChoosedEmote()
         emote.slot_id = i
         emote.emote_id = 4000 + (user_id % 20) + i
         activity.emotes.emotes.append(emote)
     
-    # Shows (field 9 - repeated uint32)
+    # Field 9: shows (repeated uint32)
     activity.shows.extend([800 + i for i in range(3)])
     
-    # PVE weapon skin (field 10)
+    # Field 10: pve_primary_weapon_skin (uint32)
     activity.pve_primary_weapon_skin = 900 + (user_id % 10)
     
-    # Ranking cards (field 11 - repeated uint32)
+    # Field 11: ranking_cards (repeated uint32)
     activity.ranking_cards.extend([1000 + i for i in range(2)])
     
-    # Pin ID (field 12)
+    # Field 12: pin_id (uint32)
     activity.pin_id = 1100 + (user_id % 5)
     
-    # Game bag show (field 13)
+    # Field 13: game_bag_show (uint32)
     activity.game_bag_show = 1200 + (user_id % 3)
     
-    # Random slots (field 14 - repeated RandomSlotInfo)
+    # Field 14: random_slots (repeated RandomSlotInfo)
     for i in range(1, 3):
         random_slot = RandomSlotInfo()
         random_slot.slot = i
@@ -77,10 +85,10 @@ def create_complete_player_activity(user_id):
         random_slot.choose_type = SlotChooseType.SlotChooseType_RANDOM if i % 2 else SlotChooseType.SlotChooseType_SINGLE
         activity.random_slots.append(random_slot)
     
-    # Title (field 15)
+    # Field 15: title (uint32)
     activity.title = 300 + (user_id % 10)
     
-    # Quick message settings (field 16 - QuickMsgSettings)
+    # Field 16: quick_msg_settings (QuickMsgSettings)
     quick_msg = QuickMsgSettings()
     quick_msg.voice = 1 + (user_id % 3)
     
@@ -93,23 +101,23 @@ def create_complete_player_activity(user_id):
     
     activity.quick_msg_settings.CopyFrom(quick_msg)
     
-    # PVE slots (field 17 - repeated PveSlotInfo)
+    # Field 17: pve_slots (repeated PveSlotInfo)
     for i in range(1, 4):
         pve_slot = PveSlotInfo()
         pve_slot.index = i
         pve_slot.skin_id = 6000 + (user_id % 30) + i
         activity.pve_slots.append(pve_slot)
     
-    # Collection actions (field 18 - repeated uint32)
+    # Field 18: collection_actions (repeated uint32)
     activity.collection_actions.extend([1300 + i for i in range(4)])
     
-    # Collection skill skins (field 19 - repeated uint32)
+    # Field 19: collection_skill_skins (repeated uint32)
     activity.collection_skill_skins.extend([1400 + i for i in range(5)])
     
-    # Loadout v2 (field 20)
+    # Field 20: load_out_v2 (uint32)
     activity.load_out_v2 = 1500 + (user_id % 10)
     
-    # Final shots (field 21 - repeated uint32)
+    # Field 21: final_shots (repeated uint32)
     activity.final_shots.extend([1600 + i for i in range(3)])
     
     # ===== Blacklist Info =====
@@ -160,55 +168,48 @@ def protobuf_to_dict(message):
     """Convert any protobuf message to a dictionary with ALL fields"""
     if isinstance(message, SelectedItems):
         return {
-            # Field 1-6: Basic appearance
+            # Field 1: avatar_id
             "avatar_id": message.avatar_id,
+            # Field 2: skin_color
             "skin_color": message.skin_color,
+            # Field 3: clothes
             "clothes": list(message.clothes),
-            "banner_id": message.banner_id,
-            "head_pic": message.head_pic,
-            
-            # Field 4: Loadouts
+            # Field 4: loadouts
             "loadouts": [{
                 "loadout_id": l.loadout_id,
                 "loadout_num": l.loadout_num,
                 "is_free_play": l.is_free_play
             } for l in message.loadouts],
-            
-            # Field 7: Slots
+            # Field 5: banner_id
+            "banner_id": message.banner_id,
+            # Field 6: head_pic
+            "head_pic": message.head_pic,
+            # Field 7: slots
             "slots": list(message.slots),
-            
-            # Field 8: Emotes
+            # Field 8: emotes
             "emotes": [{
                 "slot_id": e.slot_id,
                 "emote_id": e.emote_id
             } for e in message.emotes.emotes],
-            
-            # Field 9: Shows
+            # Field 9: shows
             "shows": list(message.shows),
-            
-            # Field 10: PVE weapon skin
+            # Field 10: pve_primary_weapon_skin
             "pve_primary_weapon_skin": message.pve_primary_weapon_skin,
-            
-            # Field 11: Ranking cards
+            # Field 11: ranking_cards
             "ranking_cards": list(message.ranking_cards),
-            
-            # Field 12: Pin ID
+            # Field 12: pin_id
             "pin_id": message.pin_id,
-            
-            # Field 13: Game bag show
+            # Field 13: game_bag_show
             "game_bag_show": message.game_bag_show,
-            
-            # Field 14: Random slots
+            # Field 14: random_slots
             "random_slots": [{
                 "slot": r.slot,
                 "skin_ids": list(r.skin_ids),
                 "choose_type": SlotChooseType.Name(r.choose_type)
             } for r in message.random_slots],
-            
-            # Field 15: Title
+            # Field 15: title
             "title": message.title,
-            
-            # Field 16: Quick message settings
+            # Field 16: quick_msg_settings
             "quick_msg_settings": {
                 "voice": message.quick_msg_settings.voice,
                 "mode_settings": [{
@@ -217,41 +218,32 @@ def protobuf_to_dict(message):
                     "roulette": list(m.roulette)
                 } for m in message.quick_msg_settings.mode_settings]
             },
-            
-            # Field 17: PVE slots
+            # Field 17: pve_slots
             "pve_slots": [{
                 "index": p.index,
                 "skin_id": p.skin_id
             } for p in message.pve_slots],
-            
-            # Field 18: Collection actions
+            # Field 18: collection_actions
             "collection_actions": list(message.collection_actions),
-            
-            # Field 19: Collection skill skins
+            # Field 19: collection_skill_skins
             "collection_skill_skins": list(message.collection_skill_skins),
-            
-            # Field 20: Loadout v2
+            # Field 20: load_out_v2
             "load_out_v2": message.load_out_v2,
-            
-            # Field 21: Final shots
+            # Field 21: final_shots
             "final_shots": list(message.final_shots)
         }
     elif isinstance(message, BlacklistInfoRes):
         return {
-            # All BlacklistInfoRes fields
             "ban_reason": EAccount_BanReason.Name(message.ban_reason),
             "expire_duration": message.expire_duration,
             "ban_time": message.ban_time,
-            # Include enum value for programmatic use
             "ban_reason_value": message.ban_reason  
         }
     elif isinstance(message, ExternalIconInfo):
         return {
-            # All ExternalIconInfo fields
             "external_icon": message.external_icon,
             "status": ExternalIconStatus.Name(message.status),
             "show_type": ExternalIconShowType.Name(message.show_type),
-            # Include enum values for programmatic use
             "status_value": message.status,
             "show_type_value": message.show_type
         }
